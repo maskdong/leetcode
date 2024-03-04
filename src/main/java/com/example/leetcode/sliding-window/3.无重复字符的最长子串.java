d7e2012f9d56c2b3eb5bb986461535214a27875b
dongjiawei <dongjiawei@baidu.com>
2024-03-04 23:39:53 +0800

    sliding window 3

--- src/main/java/com/example/leetcode/sliding-window/3.无重复字符的最长子串.java
+++ src/main/java/com/example/leetcode/sliding-window/3.无重复字符的最长子串.java
@@ -12,21 +12,25 @@ import java.util.Map;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // key是字符，value是字符对应的个数
         Map<Character, Integer> windowMap = new HashMap<>();
 
         int res = 0;
-        int left = 0, right = 0;
+        int left = 0;
+        int right = 0;
         while (right < s.length()) {
-            Character c = s.charAt(right);
+            char c = s.charAt(right);
             right++;
+            // 更新窗口内数据
             windowMap.put(c, windowMap.getOrDefault(c, 0) + 1);
 
+            // 判断是否要缩小左侧窗口
             while (windowMap.get(c) > 1) {
-                Character d = s.charAt(left);
+                char d = s.charAt(left);
                 left++;
+                // 更新窗口内数据
                 windowMap.put(d, windowMap.get(d) - 1);
             }
-
             res = Math.max(res, right - left);
         }
         return res;
