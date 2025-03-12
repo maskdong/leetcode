0e4db577726761bb4a6010750ed9426d5d7e2061
dongjiawei <dongjiawei@baidu.com>
2025-03-13 02:04:09 +0800

    更新文件权限，新增 JSON 配置文件，重构链表相关代码，删除不再使用的链表实现。

--- src/main/java/com/example/leetcode/base/TreeNode.java
+++ src/main/java/com/example/leetcode/base/TreeNode.java
@@ -8,13 +8,41 @@ public class TreeNode {
     public TreeNode() {
     }
 
-    public TreeNode(int val) {
-        this.val = val;
+    public TreeNode(int x) {
+        val = x;
     }
 
-    public TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
+    public TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
         this.left = left;
         this.right = right;
     }
+
+
+    // 按照力扣的序列化规则输入生成二叉树，返回根节点
+    public static TreeNode createRoot(Integer[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(nums[0]);
+        TreeNode[] tree = new TreeNode[nums.length];
+        tree[0] = root;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != null) {
+                tree[i] = new TreeNode(nums[i]);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (tree[i] != null) {
+                if (2 * i + 1 < nums.length) {
+                    tree[i].left = tree[2 * i + 1];
+                }
+                if (2 * i + 2 < nums.length) {
+                    tree[i].right = tree[2 * i + 2];
+                }
+            }
+        }
+        return root;
+    }
+
 }
